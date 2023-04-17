ebe2ed0a35b30423343925ce038292db93aecbce
Lukasz Romanek <lukasz.romanek@o2.pl>
2023-04-17 14:44:44 +0200

    add change code css and html

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,18 +1,1 @@
-/* Script loader */
 
-window.addEventListener('load', () => {
-	const loader = document.querySelector('.loader');
-	const header = document.querySelector('.header');
-	const sections = document.querySelectorAll('section');
-	const footer = document.querySelector('.footer');
-
-	loader.classList.add('hiddenElement');
-
-	header.classList.remove('hiddenElement');
-
-	sections.forEach((section) => {
-		section.classList.remove('hiddenElement');
-	});
-
-	footer.classList.remove('hiddenElement');
-});
