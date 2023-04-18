376c47b430888b845f58008925a2c6301134a377
Lukasz Romanek <lukasz.romanek@o2.pl>
2023-04-18 20:17:08 +0200

    change styles in section how to work

--- dev/js/components/stickyPhone.js
+++ dev/js/components/stickyPhone.js
@@ -1,15 +1,22 @@
 const stickyPanel = () => {
 	const stickyPhone = document.querySelector('.sticky-phone');
+	const sections = document.querySelectorAll('section');
+	const footer = document.querySelector('.footer');
 
-	window.addEventListener('scroll', () => {
-		if (window.scrollY >= 641) {
-			stickyPhone.classList.remove('hidden-phone');
-			stickyPhone.classList.add('show-phone');
-		} else {
-			stickyPhone.classList.remove('show-phone');
-			stickyPhone.classList.add('hidden-phone');
-		}
-	});
+	const animationStickyPanel = () => {
+		// if (window.scrollY >= sections[0].offsetHeight - 64) {
+		// 	stickyPhone.classList.remove('hidden-phone');
+		// 	stickyPhone.classList.add('show-phone');
+		// } else if (
+		// 	window.scrollY >= document.body.offsetHeight - (sections[7].offsetHeight + footer.offsetHeight) - 64 ||
+		// 	window.scrollY <= sections[0].offsetHeight - 64
+		// ) {
+		// 	stickyPhone.classList.remove('show-phone');
+		// 	stickyPhone.classList.add('hidden-phone');
+		// }
+	};
+
+	window.addEventListener('scroll', animationStickyPanel);
 };
 
 stickyPanel();
