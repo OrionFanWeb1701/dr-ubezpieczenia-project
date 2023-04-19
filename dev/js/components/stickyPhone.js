a1c2aeac8a950d1d1bbb29a511b815f29766e4e2
Lukasz Romanek <lukasz.romanek@o2.pl>
2023-04-19 13:00:58 +0200

    add sticky box

--- dev/js/components/stickyPhone.js
+++ dev/js/components/stickyPhone.js
@@ -4,10 +4,14 @@ const stickyPanel = () => {
 	const footer = document.querySelector('.footer');
 
 	const animationStickyPanel = () => {
-		// if (window.scrollY >= sections[0].offsetHeight - 64) {
-		// 	stickyPhone.classList.remove('hidden-phone');
-		// 	stickyPhone.classList.add('show-phone');
-		// } else if (
+		if (window.scrollY >= sections[0].offsetHeight - 64) {
+			stickyPhone.classList.remove('hidden-phone');
+			stickyPhone.classList.add('show-phone');
+		} else {
+			stickyPhone.classList.remove('show-phone');
+			stickyPhone.classList.add('hidden-phone');
+		}
+		// else if (
 		// 	window.scrollY >= document.body.offsetHeight - (sections[7].offsetHeight + footer.offsetHeight) - 64 ||
 		// 	window.scrollY <= sections[0].offsetHeight - 64
 		// ) {
