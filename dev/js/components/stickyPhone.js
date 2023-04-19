68e323f75ea8bb605a656bc6f9ee7a7f604f01a4
Lukasz Romanek <lukasz.romanek@o2.pl>
2023-04-19 13:58:15 +0200

    change style horizontal heading button

--- dev/js/components/stickyPhone.js
+++ dev/js/components/stickyPhone.js
@@ -1,23 +1,15 @@
 const stickyPanel = () => {
 	const stickyPhone = document.querySelector('.sticky-phone');
-	const sections = document.querySelectorAll('section');
-	const footer = document.querySelector('.footer');
+	const sectionHeading = document.querySelector('.section-heading');
 
 	const animationStickyPanel = () => {
-		if (window.scrollY >= sections[0].offsetHeight - 64) {
+		if (window.scrollY >= sectionHeading.offsetHeight - 64) {
 			stickyPhone.classList.remove('hidden-phone');
 			stickyPhone.classList.add('show-phone');
 		} else {
 			stickyPhone.classList.remove('show-phone');
 			stickyPhone.classList.add('hidden-phone');
 		}
-		// else if (
-		// 	window.scrollY >= document.body.offsetHeight - (sections[7].offsetHeight + footer.offsetHeight) - 64 ||
-		// 	window.scrollY <= sections[0].offsetHeight - 64
-		// ) {
-		// 	stickyPhone.classList.remove('show-phone');
-		// 	stickyPhone.classList.add('hidden-phone');
-		// }
 	};
 
 	window.addEventListener('scroll', animationStickyPanel);
